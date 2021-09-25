261547360dd80fa2eec6a21a1a5a2c25529867b6
Lavanya Jain <ljain@lenovo.com>
2021-09-25 16:29:11 +0530

    Run script and generate file

--- solution/gencsv.sh
+++ solution/gencsv.sh
@@ -1,4 +1,7 @@
-#!/bin/bash +e
+#!/bin/bash 
+
+#remove if file with name inputFile exists and also dont fail the script if inputFile doesn't exists
+rm inputFile || true
 
 #create file
 touch inputFile
@@ -7,5 +10,4 @@ for ((i=0;i<10;i+=1))
 do
  echo "${i}, $RANDOM" >> inputFile
 done
-while true; do sleep 1; done
 
